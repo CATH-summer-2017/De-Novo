89f35ed807be6b8605e371b50ac1407a19ef2ba4
Ilya Senatorov <il.senatorov@gmail.com>
2017-10-05 10:34:19 +0100

    added the webpage loader back

---
+++ scripts/webpage_loader.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import requests
+from pprint import pprint
+u_a = 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+def get_page():
+    res = requests.get("http://download.cathdb.info/cath/releases/latest-release/cath-classification-data/cath-domain-list.txt", headers={"USER-AGENT":u_a})
+    return res.text
+txt_file = open("/home/ilya/Projects/De-Novo/data/alldoms.txt", "w")
+txt_file.write(get_page())
